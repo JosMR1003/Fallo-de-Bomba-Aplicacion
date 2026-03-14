3278e30c1c02255e86881252395d60ec49e02b62
JosMR1003 <josmroca@espol.edu.ec>
2026-03-14 12:13:22 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+# Este comando mágico guarda el contenido de esta celda en un archivo llamado 'main.py'
+
+# --- Importación de Librerías ---
+import joblib  # Para cargar nuestros modelos guardados (.pkl)
+import streamlit as st  # La librería principal para crear la aplicación web
+import pandas as pd  # Para manejar los datos de entrada en un DataFrame
+import altair as alt  # Para crear gráficos interactivos
